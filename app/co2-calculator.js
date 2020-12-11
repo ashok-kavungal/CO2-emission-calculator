c349d7ead88e713e23f33803ff418336db2ebeca
ashokkavungal5@gmail.com <ashokkavungal5@gmail.com>
2020-12-11 20:10:08 +0100

    refactored

--- app/co2-calculator.js
+++ app/co2-calculator.js
@@ -1,13 +1,13 @@
 const emissonCalculator = require('./calculator');
-const input = require('./validate');
+const input = require('./cli');
 
-if(input.commands){
-    (async () => {
-        const validInput = input.commands.argv;
-        const emisson = new emissonCalculator(validInput);
-        const finalResult = await emisson.calculate();
-        console.log(finalResult)
-    })().catch(err => {
-        console.error(err);
-    });   
+if (input.commands) {
+  (async () => {
+    const validInput = input.commands.argv;
+    const emisson = new emissonCalculator(validInput);
+    const finalResult = await emisson.calculate();
+    console.log(finalResult);
+  })().catch((err) => {
+    console.error(err);
+  });
 }
--- app/test/app.test.js
+++ app/test/app.test.js
@@ -1,99 +1,102 @@
-const input = require('../validate');
+const input = require('../cli');
 const emissonCalculator = require('../calculator');
 const emissionList = require('../data');
 
-
 test('Functional requirement 1', () => {
-  const args = ['--transportation-method',
+  const args = [
+    '--transportation-method',
     'medium-diesel-car',
     '--distance',
     '15',
     '--unit-of-distance',
-    'km'
+    'km',
   ];
   const validInput = input.commands.parse(args);
   const emisson = new emissonCalculator(validInput);
-  emisson.calculate().then(result => {
+  emisson.calculate().then((result) => {
     expect(result).toBe('Your trip caused 2.6kg of CO2-equivalent.');
   });
 });
 
-
 test('Functional requirement 2', () => {
-  const args = ['--transportation-method',
+  const args = [
+    '--transportation-method',
     'large-petrol-car',
     '--distance',
-    '1800.5'
+    '1800.5',
   ];
   const validInput = input.commands.parse(args);
   const emisson = new emissonCalculator(validInput);
-  emisson.calculate().then(result => {
+  emisson.calculate().then((result) => {
     expect(result).toBe('Your trip caused 507.7kg of CO2-equivalent.');
   });
 });
 
 test('Functional requirement 3', () => {
-  const args = ['--transportation-method',
+  const args = [
+    '--transportation-method',
     'train',
     '--distance',
     '14500',
     '--unit-of-distance',
     'm',
     '--output',
-    'g'
+    'g',
   ];
   const validInput = input.commands.parse(args);
   const emisson = new emissonCalculator(validInput);
-  emisson.calculate().then(result => {
+  emisson.calculate().then((result) => {
     expect(result).toBe('Your trip caused 87g of CO2-equivalent.');
   });
 });
 
 test('Functional requirement 4', () => {
-  const args = ['--transportation-method',
+  const args = [
+    '--transportation-method',
     'train',
     '--distance',
     '14500',
     '--unit-of-distance',
     'm',
     '--output',
-    'kg'
+    'kg',
   ];
   const validInput = input.commands.parse(args);
   const emisson = new emissonCalculator(validInput);
-  emisson.calculate().then(result => {
+  emisson.calculate().then((result) => {
     expect(result).toBe('Your trip caused 0.1kg of CO2-equivalent.');
   });
 });
 
 test('Functional requirement 5', () => {
-  const args = ['--transportation-method',
+  const args = [
+    '--transportation-method',
     'medium-diesel-car',
     '--distance',
     '15',
     '--unit-of-distance',
-    'km'
+    'km',
   ];
   const validInput = input.commands.parse(args);
   const emisson = new emissonCalculator(validInput);
-  emisson.calculate().then(result => {
+  emisson.calculate().then((result) => {
     expect(result).toBe('Your trip caused 2.6kg of CO2-equivalent.');
   });
 });
 
-
 it('Transportation method not found', async () => {
-  const args = ['--transportation-method',
+  const args = [
+    '--transportation-method',
     'somemethod',
     '--distance',
     '15',
     '--unit-of-distance',
-    'km'
+    'km',
   ];
   const validInput = input.commands.parse(args);
   const emisson = new emissonCalculator(validInput);
   const data = emissionList.data;
-  await expect(emisson.fetchRate(data,emisson.vehicle))
-        .rejects.toEqual('The vehicle not found in data list.');
+  await expect(emisson.fetchRate(data, emisson.vehicle)).rejects.toEqual(
+    'The vehicle not found in data list.'
+  );
 });
-
