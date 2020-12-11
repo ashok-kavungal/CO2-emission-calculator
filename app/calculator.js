ebeb3480f70e9e33d3c18d9fa91ce3256ce157fe
ashokkavungal5@gmail.com <ashokkavungal5@gmail.com>
2020-12-11 17:31:22 +0100

    changed to json file

--- app/calculator.js
+++ app/calculator.js
@@ -1,66 +1,66 @@
-const emissionList = require('./data');
+const data = require('./data');
 
 class CoCalculator {
-    constructor(cmd) {
-        this.vehicle = cmd['transportation-method'];
-        this.distance = cmd['distance'];
-        this.distUnit = cmd['unit-of-distance'];
-        this.outputUnit = cmd['output'];
-    }
+  constructor(cmd) {
+    this.vehicle = cmd['transportation-method'];
+    this.distance = cmd['distance'];
+    this.distUnit = cmd['unit-of-distance'];
+    this.outputUnit = cmd['output'];
+  }
 
-    roundValue = n => Math.round(n * 10) / 10;
+  roundValue = (n) => Math.round(n * 10) / 10;
 
-    getResult = rate => {
-        //use the fetched rate and calculates the final emission value
-        // returns the final string as result
-        let outputval = this.distUnit === 'km' ?
-            this.outputUnit === 'kg' ?
-            (this.roundValue(this.distance * (rate / 1000)) + 'kg') :
-            (this.roundValue(this.distance * rate) + 'g') :
-            this.outputUnit === 'kg' ?
-            (this.roundValue(this.distance * (rate / 1000000)) + 'kg') :
-            (this.roundValue(this.distance * (rate / 1000)) + 'g')
-        const result = `Your trip caused ${outputval} of CO2-equivalent.`
-        return result;
-    }
+  getResult = (rate) => {
+    //use the fetched rate and calculates the final emission value
+    // returns the final string as result
+    let outputval =
+      this.distUnit === 'km'
+        ? this.outputUnit === 'kg'
+          ? this.roundValue(this.distance * (rate / 1000)) + 'kg'
+          : this.roundValue(this.distance * rate) + 'g'
+        : this.outputUnit === 'kg'
+        ? this.roundValue(this.distance * (rate / 1000000)) + 'kg'
+        : this.roundValue(this.distance * (rate / 1000)) + 'g';
+    const result = `Your trip caused ${outputval} of CO2-equivalent.`;
+    return result;
+  };
 
-    fetchRate(data, vehicle) {
-        //search the nested object using an recursive fn
-        //resolves the emission value in gram per km if vehicle is found in list
-        //rejects if the transportation method is not found in data list
-        return new Promise((resolve,reject) => {
+  fetchRate(data, vehicle) {
+    //search the nested object using an recursive fn
+    //resolves the emission value in gram per km if vehicle is found in list
+    //rejects if the transportation method is not found in data list
+    return new Promise((resolve, reject) => {
+      let found = false;
+      let recursion = 0;
 
-            let found = false;
-            let recursion = 0;
-            
-            const searchKeyvalue = (obj, key) => {
-                recursion += 1;
-                for (let prop in obj) {
-                    if (found) break;
-                    if (typeof (obj[prop]) == "object") {
-                        searchKeyvalue(obj[prop], key);
-                    } else {
-                        if (prop == key) {
-                            found = true;
-                            resolve(obj[key])
-                        }
-                    }
-                }
-                recursion -= 1;
-                if (recursion === 0 && !found) {
-                    reject('The vehicle not found in data list.');
-                }
+      const searchKeyvalue = (obj, key) => {
+        recursion += 1;
+        for (let prop in obj) {
+          if (found) break;
+          if (typeof obj[prop] == 'object') {
+            searchKeyvalue(obj[prop], key);
+          } else {
+            if (prop == key) {
+              found = true;
+              resolve(obj[key]);
             }
-            searchKeyvalue(data, vehicle);
-        });
-    }
+          }
+        }
+        recursion -= 1;
+        if (recursion === 0 && !found) {
+          reject('The vehicle not found in data list.');
+        }
+      };
+      searchKeyvalue(data, vehicle);
+    });
+  }
 
-    async calculate() {
-        const data = emissionList.data;
-        const rate = await this.fetchRate(data, this.vehicle);
-        const result = this.getResult(rate);
-        return result;
-    }
+  async calculate() {
+    const rate = await this.fetchRate(data, this.vehicle);
+    console.log('the rate is :' + rate);
+    const result = this.getResult(rate);
+    return result;
+  }
 }
 
 module.exports = CoCalculator;
